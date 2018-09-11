8924fb263b49421e916e3ef853569a11a462418c
Bob Petersen <33petersen@gmail.com>
2018-09-11 09:18:22 -0500

    nav template setup

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    return (
+			<div>
+			</div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Header);
