00c40151475351dbf63a9d5bc58ea819d4618ddd
Bob Petersen <33petersen@gmail.com>
2018-09-13 12:07:04 -0500

    component folder structure set up

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch,} from 'react-router-dom';
+import { connect } from 'react-redux';
+import Header from './components/Header/Header';
